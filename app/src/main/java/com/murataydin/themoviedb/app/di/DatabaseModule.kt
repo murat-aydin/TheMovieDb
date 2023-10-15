a32ef5221c8d04032a8046b611b25bc3b2fc18b2
Murat Aydın <murat.aydin@loodos.com>
2023-10-16 02:24:39 +0300

    fix di module for new struct

--- app/src/main/java/com/murataydin/themoviedb/app/di/DatabaseModule.kt
+++ app/src/main/java/com/murataydin/themoviedb/app/di/DatabaseModule.kt
@@ -1,11 +1,30 @@
 package com.murataydin.themoviedb.app.di
 
+import android.content.Context
+import androidx.room.Room
+import com.murataydin.themoviedb.BuildConfig
+import com.murataydin.themoviedb.data.local.db.MovieDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provide(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, MovieDatabase::class.java, BuildConfig.APPLICATION_ID + ".db"
+    )
+        .allowMainThreadQueries()
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideMovieDBDao(db: MovieDatabase) = db.movieDBDao()
 
 }
